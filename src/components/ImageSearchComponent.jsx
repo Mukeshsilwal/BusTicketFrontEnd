11ff8fe2590388b6d47dd2f262c92e677e22dd33
RahulGautamSingh <rahultesnik@gmail.com>
2024-03-14 16:40:33 +0545

    fetch busStops from bacnend

--- src/components/ImageSearchComponent.jsx
+++ src/components/ImageSearchComponent.jsx
@@ -1,21 +1,8 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ImageSearchComponent.css";
 import axiosInstance from "../functions";
 import BusListContext from "../context/busdetails";
-
-const nepalCities = [
-  "Kathmandu",
-  "Pokhara",
-  "Biratnagar",
-  "Lalitpur",
-  "Bharatpur",
-  "Birgunj",
-  "Dharan",
-  "Bhaktapur",
-  "Janakpur",
-  "Butwal",
-];
 
 const ImageSearchComponent = () => {
   let navigate = useNavigate();
@@ -24,6 +11,7 @@ const ImageSearchComponent = () => {
   const [destination, setDestination] = useState("");
   const [date, setDate] = useState("");
   const { setBusList } = useContext(BusListContext);
+  const [busStops, setBusStops] = useState([]);
 
   const handleSourceChange = (e) => {
     setSource(e.target.value);
@@ -57,50 +45,73 @@ const ImageSearchComponent = () => {
   // Get today's date in the format YYYY-MM-DD
   const today = new Date().toISOString().split("T")[0];
 
+  async function getBusStops() {
+    const busStopRes = await fetch("http://localhost:8089/busStop/get");
+    if (busStopRes.ok) {
+      const busStops = await busStopRes.json();
+      console.log(busStops);
+      setBusStops(busStops);
+    }
+  }
+
+  useEffect(() => {
+    getBusStops();
+  }, []);
   return (
     <div className="flex h-full pt-8 bg-cyan-700">
-      <div className="image-container">
-        <img
-          src="https://img.freepik.com/premium-vector/simple-buss-traansportation-logo-design_569344-386.jpg?w=2000"
-          alt="Image"
-        />
-      </div>
-      <div className="w-2/5 flex-col self-center justify-self-center">
-        <select onChange={handleSourceChange} className="w-7/10 m-2 p-2">
-          <option value="">Select source</option>
-          {nepalCities.map((city) => (
-            <option key={city} value={city} onChange={() => setSource(city)}>
-              {city}
-            </option>
-          ))}
-        </select>
-        <select onChange={handleDestinationChange} className="w-7/10 m-2 p-2">
-          <option value="">Select destination</option>
-          {nepalCities.map((city) => (
-            <option
-              key={city}
-              value={city}
-              onChange={() => setDestination(city)}
+      {busStops.length > 0 && (
+        <>
+          <div className="image-container">
+            <img
+              src="https://img.freepik.com/premium-vector/simple-buss-traansportation-logo-design_569344-386.jpg?w=2000"
+              alt="Image"
+            />
+          </div>
+          <div className="w-2/5 flex-col self-center justify-self-center">
+            <select  onChange={handleSourceChange} className="w-7/10 m-2 p-2">
+              <option value="">Select source</option>
+              {busStops.map((city) => (
+                <option
+                  key={city.name}
+                  value={city.name}
+                  onChange={() => setSource(city.name)}
+                >
+                  {city.name}
+                </option>
+              ))}
+            </select>
+            <select
+              onChange={handleDestinationChange}
+              className="w-7/10 m-2 p-2"
             >
-              {city}
-            </option>
-          ))}
-        </select>
-        <input
-          type="date"
-          placeholder="Date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          min={today}
-          className="block w-7/10 m-2 p-2"
-        />
-        <button
-          onClick={handleSearch}
-          className="px-5 py-3 border-none bg-black text-white cursor-pointer"
-        >
-          Search
-        </button>
-      </div>
+              <option value="">Select destination</option>
+              {busStops.map((city) => (
+                <option
+                  key={city.name}
+                  value={city.name}
+                  onChange={() => setDestination(city.name)}
+                >
+                  {city.name}
+                </option>
+              ))}
+            </select>
+            <input
+              type="date"
+              placeholder="Date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              min={today}
+              className="block w-7/10 m-2 p-2"
+            />
+            <button
+              onClick={handleSearch}
+              className="px-5 py-3 border-none bg-black text-white cursor-pointer"
+            >
+              Search
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
