370018ae113a57cd7924b51b9ed567837df58091
RahulGautamSingh <rahultesnik@gmail.com>
2024-03-16 06:52:07 +0545

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,48 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import { useState } from "react";
 
 const NavigationBar = () => {
   const [page, setPage] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pageUrl = location.pathname;
 
   return (
     <div className="flex justify-between px-10 py-2 bg-cyan-950 text-white fixed w-full top-0 left-0 h-fit">
       <div className="logo">
-        {/* Replace 'YourLogo' with your actual logo */}
-        <img src="./bus.svg" alt="Logo" />
+        {/* <img src="./bus.svg" alt="Logo" /> */}
+        <p className="text-2xl">BusSewa</p>
       </div>
       {page === "admin" && <Navigate to={"/admin/login"} />}
       {page === "homepage" && <Navigate to={"/"} />}
       <div className="nav-buttons">
+        {!pageUrl.includes("admin/panel") && (
+          <button
+            onClick={() => {
+              setPage("admin");
+            }}
+          >
+            Admin Login
+          </button>
+        )}
+        {pageUrl.includes("admin/panel") && (
+          <button
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/");
+            }}
+          >
+            Admin Logout
+          </button>
+        )}
         <button
           onClick={() => {
-            setPage("admin");
+            setPage("homepage");
           }}
         >
-          Admin Login
+          HomePage
         </button>
-        <button
-        onClick={() => {
-          setPage("homepage");
-        }}
-        >HomePage</button>
-        <button>Button 3</button>
       </div>
     </div>
   );
