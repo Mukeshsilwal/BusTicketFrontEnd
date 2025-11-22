5d9dbaff6a2dfd37ab117eae64a92d01827b8d6b
mukesh-silwal <mukesh.silwal@f1soft.com>
2025-11-22 07:43:51 +0545

    final code enhanced

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:8089',
+  BASE_URL: process.env.REACT_APP_API_URL,
   ENDPOINTS: {
     // Auth endpoints
     LOGIN: '/auth/login',
@@ -15,6 +15,8 @@ const API_CONFIG = {
     // Route endpoints
     GET_ROUTES: '/route/get',
     CREATE_ROUTE: '/admin/busStopRoute',
+
+    SEARCH_BUSES: "/bus/search",
     
     // Bus endpoints
     GET_ALL_BUSES: '/bus/route',
